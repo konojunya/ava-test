cc793ecd49f5d9795d3389190fa59e818fd22ad6
konojunya <junya03dance@gmail.com>
2017-12-12 03:27:10 +0900

    かききったヨ

--- test/hoge.js
+++ test/hoge.js
@@ -3,3 +3,20 @@ import test from 'ava'
 test("タイトル", (t) => {
   t.pass()
 })
+
+test("1 + 1 = 2", (t) => {
+  const a = 1
+  const b = 1
+
+  t.is(a + b, 2)
+})
+
+test("async test", async (t) => {
+  const res = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("hoge")
+    }, 5 * 1000)
+  })
+
+  t.is(res, "hoge")
+})
